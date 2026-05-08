d1a21fb7fb28f0e42d5ab08c43edf163a77b1aa9
alexiasandor <sandor.alexia16@gmail.com>
2026-05-08 12:24:11 +0300

    project structure

---
+++ TrainTicketingApp/src/main/java/com/trainTicketingApp/TrainTicketingApp/TrainTicketingAppApplication.java
@@ -0,0 +1,13 @@
+package com.trainTicketingApp.TrainTicketingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTicketingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainTicketingAppApplication.class, args);
+	}
+
+}
---
+++ TrainTicketingApp/src/test/java/com/trainTicketingApp/TrainTicketingApp/TrainTicketingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainTicketingApp.TrainTicketingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
